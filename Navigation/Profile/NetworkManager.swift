3db64c9029d7e9041a77888d04d509574e6690a2
Evgeny Drozdov <cyberhell@mail.ru>
2023-04-16 12:43:39 +1000

    hw2 done

--- Navigation/Profile/NetworkManager.swift
+++ Navigation/Profile/NetworkManager.swift
@@ -7,25 +7,25 @@
 
 import UIKit
 
-// Для обращения в сеть по адресу
+// Для обращения в сеть по адресу, Задание 1
 struct NetworkService {
-
+    
     static func request(for configuration: AppConfiguration) {
-
+        
         // создаем сессию по умолчанию
         let session = URLSession(configuration: .default)
-
+        
         // создаем строку подключения
         let url = URL(string: configuration.rawValue)
-
+        
         // создаем задание
         let task = session.dataTask(with: url!) { data, response, error in
-
+            
             // Обрабатываем ошибку
             if let error = error {
                 print(error.localizedDescription)
             }
-
+            
             let statusCode = (response as? HTTPURLResponse)?.statusCode
             if statusCode != 200 {
                 print("There's error with data, error code is \(String(describing: statusCode))")
@@ -36,33 +36,33 @@ struct NetworkService {
                 } else {
                     print("Current fileds for HTTP is nil")
                 }
-
+                
                 if let contentTypeAnswer = (response as? HTTPURLResponse)?.allHeaderFields["Content-Type"] as? String {
                     print("Current fileds for Content-Type is", contentTypeAnswer)
                 } else {
                     print("Current fileds for Content-Type is nil")
                 }
-
+                
                 if let varyAnswer = (response as? HTTPURLResponse)?.allHeaderFields["Vary"] as? String {
                     print("Current fileds for Vary is", varyAnswer)
                 } else {
                     print("Current fileds for Vary is nil")
                 }
-
+                
                 if let allowAnswer = (response as? HTTPURLResponse)?.allHeaderFields["Allow"] as? String {
                     print("Current fileds for Allow is", allowAnswer)
                 } else {
                     print("Current fileds for Allow is nil")
                 }
             }
-
+            
             if data == nil {
                 print("There's nothing to show")
                 return
             } else {
                 print(data!.description)
             }
-
+            
             do {
                 if let answer = try JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     // ловим ответ и парсим
@@ -74,22 +74,123 @@ struct NetworkService {
             } catch {
                 print("Поймана ошибка при попытке парса данных", error.localizedDescription)
             }
-
+            
         }
-
+        
         task.resume()
-
+        
     }
-
+    
+    // чтобы не плодить сущности для 2 задачи делаю тут реализацию
+    
+    static func requestAnotherAPI(for url: String, comletion: ((_ titleValue: String?) -> Void)?) {
+        
+        let session = URLSession(configuration: .default)
+        
+        let url = URL(string: url)
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            
+            if let error = error {
+                print(String(describing: error))
+            }
+            
+            let statusCode = (response as? HTTPURLResponse)?.statusCode
+            if statusCode != 200 {
+                print(String(describing: statusCode))
+            }
+            
+            if data == nil {
+                print("There's nothing to show")
+            }
+            
+            do {
+                if let answer = try JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                    let title = answer["title"] as? String ?? ""
+                    comletion?(title)
+                }
+            } catch {
+                print("при попытке парса данных произошла ошибка ", error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    static func requestPlanetAPI(for url: String, comletion: ((_ orbitalPeriod: String?) -> Void)?) {
+        let session = URLSession(configuration: .default)
+        
+        let url = URL(string: url)
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            let statusCode = (response as? HTTPURLResponse)?.statusCode
+            if statusCode != 200 {
+                print(String(describing: statusCode))
+            }
+            
+            if data == nil {
+                print("There's nothing to show")
+            }
+            
+            do {
+                let orbitalPeriod = try JSONDecoder().decode(Planet.self, from: data!)
+                comletion?(orbitalPeriod.orbitalPeriod)
+            } catch {
+                print("ошибка парсинга данных ", String(describing: error))
+            }
+        }
+        task.resume()
+    }
 }
 
 // Перечисление делаем для значений URL
 // Чтобы можно было выбрать рандомно кейс из него
 // Добавим протокол CaseIterable
 enum AppConfiguration: String, CaseIterable {
-
+    
     case people = "https://swapi.dev/api/people/8"
     case starship = "https://swapi.dev/api/starships/3"
     case planets = "https://swapi.dev/api/planets/5"
+    
+}
 
+// Делаем модель данных для планет
+struct Planet: Decodable {
+    var name: String
+    var rotation_period: String
+    // отображать будем его
+    var orbitalPeriod: String
+    var diameter: String
+    var climate: String
+    var gravity: String
+    var terrain: String
+    var surface_water: String
+    var population: String
+    var residents: [String]
+    var films: [String]
+    var created: String
+    var edited: String
+    var url: String
+    
+    // Добавляем отображение имени для замены (не понятно зачем, но по заданию просили)
+    enum CodingKeys: String, CodingKey {
+        case name
+        case rotation_period
+        // как хочу = какой ключ в API
+        case orbitalPeriod = "orbital_period"
+        case diameter
+        case climate
+        case gravity
+        case terrain
+        case surface_water
+        case population
+        case residents
+        case films
+        case created
+        case edited
+        case url
+    }
 }
--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         if #available(iOS 15, *) {
@@ -19,73 +19,81 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             appearence.configureWithOpaqueBackground()
             UINavigationBar.appearance().standardAppearance = appearence
             UINavigationBar.appearance().scrollEdgeAppearance = appearence
-            }
+        }
         
         guard let windowScene  = (scene as? UIWindowScene) else { return }
         
-
+        
         LogInViewController.loginFactoryDelegate = MyLoginFactory()
         
         self.window = UIWindow(windowScene: windowScene)
         let userFeedController = UINavigationController(rootViewController: FeedViewController())
         let loginViewController = UINavigationController(rootViewController: LogInViewController())
+        let infoViewController = UINavigationController(rootViewController: InfoViewController())
         let tabBarController = UITabBarController()
+      
         
-        tabBarController.viewControllers = [userFeedController, loginViewController]
-        tabBarController.viewControllers?.enumerated().forEach {
-            $1.tabBarItem.title = $0 == 0 ? "Feed" : "Profile"
-            $1.tabBarItem.image = $0 == 0
-                    ? UIImage(systemName: "list.bullet.rectangle.portrait.fill")
-                    : UIImage(systemName: "brain.head.profile")
-        }
+        tabBarController.viewControllers?[0].tabBarItem.title = "Feed"
+        tabBarController.viewControllers?[1].tabBarItem.title = "Profile"
+        tabBarController.viewControllers?[2].tabBarItem.title = "Info"
+        
+        tabBarController.tabBar.items?[0].image = UIImage(systemName: "list.bullet.rectangle.portrait.fill")
+        tabBarController.tabBar.items?[1].image = UIImage(systemName: "brain.head.profile")
+        tabBarController.tabBar.items?[2].image = UIImage(systemName: "info.circle")
+        
+        tabBarController.tabBar.backgroundColor = UIColor.white
+        
+        tabBarController.viewControllers = [userFeedController, loginViewController, infoViewController]
+//        tabBarController.viewControllers?.enumerated().forEach {
+//            $1.tabBarItem.title = $0 == 0 ? "Feed" : "Profil"
+//            $1.tabBarItem.image = $0 == 0
+//            ? UIImage(systemName: "list.bullet.rectangle.portrait.fill")
+//            : UIImage(systemName: "brain.head.profile")
+//        }
+        
         self.window?.rootViewController = tabBarController
         self.window?.makeKeyAndVisible()
-        
-
-        if let TB = window?.rootViewController as? UITabBarController,
-                    let navi = TB.viewControllers?.last as? UINavigationController,
-           let VC = navi.viewControllers.first as? LogInViewController {
-            VC.loginDelegate = LoginInspector()
             
-            // Создаем рандомную ссылку для обращения к API
-                    let randomValueForApi = AppConfiguration.allCases.randomElement()!
-                    print("Ссылка, которую сформировали рандомно - ", randomValueForApi.rawValue)
-
-                    // Передаем ссылку в сервис для обращения к API
-                    NetworkService.request(for: randomValueForApi)
         }
-    
-
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+        
+            
+            //            // Создаем рандомную ссылку для обращения к API
+            //                    let randomValueForApi = AppConfiguration.allCases.randomElement()!
+            //                    print("Ссылка, которую сформировали рандомно - ", randomValueForApi.rawValue)
+            //
+            //                    // Передаем ссылку в сервис для обращения к API
+            //                    NetworkService.request(for: randomValueForApi)
+            //        }
+            
+        
+        func sceneDidDisconnect(_ scene: UIScene) {
+            // Called as the scene is being released by the system.
+            // This occurs shortly after the scene enters the background, or when its session is discarded.
+            // Release any resources associated with this scene that can be re-created the next time the scene connects.
+            // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        }
+        
+        func sceneDidBecomeActive(_ scene: UIScene) {
+            // Called when the scene has moved from an inactive state to an active state.
+            // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        }
+        
+        func sceneWillResignActive(_ scene: UIScene) {
+            // Called when the scene will move from an active state to an inactive state.
+            // This may occur due to temporary interruptions (ex. an incoming phone call).
+        }
+        
+        func sceneWillEnterForeground(_ scene: UIScene) {
+            // Called as the scene transitions from the background to the foreground.
+            // Use this method to undo the changes made on entering the background.
+        }
+        
+        func sceneDidEnterBackground(_ scene: UIScene) {
+            // Called as the scene transitions from the foreground to the background.
+            // Use this method to save data, release shared resources, and store enough scene-specific state information
+            // to restore the scene back to its current state.
+        }
+        
 }
+    
 
